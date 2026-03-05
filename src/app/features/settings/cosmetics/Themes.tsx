f605a71058c509ef8d90f317b6c420c6cf2fc710
Didi <didi@didi.party>
2026-03-05 22:55:19 +1030

    Text Updates

--- src/app/features/settings/cosmetics/Themes.tsx
+++ src/app/features/settings/cosmetics/Themes.tsx
@@ -339,8 +339,8 @@ export function Appearance() {
 
         <SequenceCard className={SequenceCardStyle} variant="SurfaceVariant" direction="Column">
           <SettingTile
-            title="Use Cinny Font"
-            description="Use Nunito (the original Cinny font) instead of Inter."
+            title="Use Inter"
+            description="Use Inter (the font used in Cinny font) instead of Nunito."
             after={<Switch variant="Primary" value={useCinnyFont} onChange={setUseCinnyFont} />}
           />
         </SequenceCard>
